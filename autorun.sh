41309cf6ff47163e4029bf4a49fac346ebe552c6
Vladimir LAVALLADE <erus.iluvatar@gmail.com>
2010-08-10 18:33:57 +0200

    A bunch of crap

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,5 @@
 #!/bin/dash
-#feh --bg-center /home/erus/.walls/conky.png&
-feh --bg-center /home/erus/.walls/conky_new.png&
+feh --bg-center /home/erus/.walls/conky.png&
 
 if [ `pidof urxvtd | wc -l` -lt 1 ] ; then
 urxvtd -q -f -o
@@ -8,7 +7,7 @@ urxvtc
 wmfs -c tag 3
 urxvtc
 wmfs -c screen_prev_sel
-urxvtc -name Irc -e ssh erus@compaq -t 'tmux -lu a'
+urxvtc -name Irc -e ssh erus@compaq
 wmfs -c tag 3
 urxvtc
 wmfs -c tag 1
@@ -19,7 +18,5 @@ fi
 [ `pidof conky-cli | wc -l` != 1 ] && conky-cli -c /home/erus/.config/wmfs/conkyrc >> /tmp/.conk 2> /dev/null &
 
 [ `pidof conky | wc -l` != 1 ] && conky 2> /dev/null &
-
-[ `pidof fbpanel | wc -l` != 1 ] && fbpanel &
 
 killall updates_pacman.sh ; /home/erus/.config/wmfs/updates_pacman.sh &
